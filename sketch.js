5a7fb8d8d25651d74c6277579e596f2d6d476094
bobastraw <izulicant@gmail.com>
2022-01-20 18:33:07 -0600

    commit

---
+++ sketch.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+
+
+
+
+console.log("print to console");
+
+
+
+function draw() {
+  background(0,200,100);
+  
+  fill('red');
+  arc(280, 250, 400, 350, 650, 0, + QUARTER_PI, OPEN);
+  
+    
+  fill(250)
+  circle(150,150,50);
+  circle(300,200,60)
+  circle(200,250,25)
+  circle()
+
+
+
+}
