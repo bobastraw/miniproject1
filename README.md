c428c7b5a3880cc818f116a5e4a690fd7acfdd79
bobastraw <izulicant@gmail.com>
2022-01-21 20:58:22 -0600

    mushroom

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # miniproject1
-First P5 sketch
-idk
+The background is green. In the middle of the window a red half circle with a downwards arch with the width of 600 and a height of 500. On top of the half circle, ten white circles of various sizes scattered on its surface. Centered underneath the half circle, a tan rectangle with rounded edges with a width of 230 and a height of 230. All the shapes have a black outline.        
--- sketch.js
+++ sketch.js
@@ -1,27 +1,34 @@
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(windowWidth, windowHeight);
 }
-
-
-
 
 
 console.log("print to console");
 
 
-
 function draw() {
   background(0,200,100);
-  
+
+  strokeWeight(4);
   fill('red');
-  arc(280, 250, 400, 350, 650, 0, + QUARTER_PI, OPEN);
+  arc(700, 250, 600, 500, 650, 100 + QUARTER_PI, CHORD);
   
     
   fill(250)
-  circle(150,150,50);
-  circle(300,200,60)
-  circle(200,250,25)
-  circle()
+  circle(460,235,35);
+  circle(510,140,110);
+  circle(550,290,75);
+  circle(670,315,35);
+  circle(670,200,65);
+  circle(670,60,85);
+  circle(830,100,50);
+  circle(820,255,130);
+  circle(900,175,35);
+  circle(950,270,55);
+
+  fill(255,204,150);
+  rect(580, 335, 230, 230, 20);
+  
 
 
 
